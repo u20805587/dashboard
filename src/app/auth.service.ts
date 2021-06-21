45f56119c25807be5f3e98c1537ada860d169ce7
u20805587 <u20805587@tuks.co.za>
2021-06-21 05:20:04 +0200

    Commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Observable, of } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class AuthService {
+
+  readonly baseUrl = 'http://localhost:8080/api/access';
+
+  constructor(private http: HttpClient) {
+  }
+   isUserLoggedIn: boolean = false;
+   currentUser: string = '';
+
+   login(userName: string, password: string): Observable<any> {
+      console.log(userName);
+      console.log(password);
+      this.isUserLoggedIn = userName == 'admin' && password == 'admin';
+      localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false");
+      localStorage.setItem('currentUser', userName);
+
+   return of(this.isUserLoggedIn).pipe(
+      delay(1000),
+      tap(val => {
+         console.log("Is User Authentication is successful: " + val);
+      })
+   );
+   }
+
+   logout(): void {
+   this.isUserLoggedIn = false;
+   this.currentUser = '';
+      localStorage.removeItem('isUserLoggedIn');
+      localStorage.removeItem('currentUser');
+   }
+
+   getAccess(functionId: string): Observable<boolean>{
+     // return this.http.getAccess(`${this.baseUrl}/${localStorage.userName}/${functionId}`);
+   }
+
+}
